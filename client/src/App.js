88ed59adee6d3bbc72efd48c753873f8a4ffcec7
Tusharshrivastav <ts3657@gmail.com>
2021-09-09 14:04:32 +0530

    infoheader

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,28 @@
-import { Box } from "@material-ui/core";
+import { Box, makeStyles } from "@material-ui/core";
 import Header from "./components/Header";
+import InfoHeader from "./components/InfoHeader";
+const useStyles = makeStyles((theme) => ({
+  container: {
+    marginTop: 110,
+    width: "59%",
+    margin: "0 auto",
+    [theme.breakpoints.down("md")]: {
+      width: "75%",
+    },
+    [theme.breakpoints.down("sm")]: {
+      width: "85%",
+    },
+  },
+}));
 const App = () => {
+  const classes = useStyles();
   return (
     <Box>
       <Header />
+      <Box className={classes.container}>
+        <InfoHeader />
+        {/* <Articles /> */}
+      </Box>
     </Box>
   );
 };
---
+++ client/src/components/InfoHeader.jsx
@@ -0,0 +1,49 @@
+import { Box, makeStyles, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    background: "#f44336",
+    color: "#FFFFFF",
+    display: "flex",
+    alignItems: "center",
+    height: 48,
+    marginBottom: 30,
+    [theme.breakpoints.down("md")]: {
+      display: "none",
+    },
+  },
+  logo: {
+    height: 34,
+    "&:last-child": {
+      margin: "0 50px 0 20px",
+    },
+  },
+  text: {
+    fontSize: 14,
+    fontWeight: 300,
+    marginLeft: 50,
+    fontFamily: "'Roboto',sans-serif",
+  },
+}));
+
+const InfoHeader = () => {
+  const classes = useStyles();
+  const appleStore =
+    "https://assets.inshorts.com/website_assets/images/appstore.png";
+  const googleStore =
+    "https://assets.inshorts.com/website_assets/images/playstore.png";
+
+  return (
+    <Box className={classes.container}>
+      <Typography className={classes.text}>
+        For the best experience use <b>inshorts</b> app on your smartphone
+      </Typography>
+      <Box style={{ marginLeft: "auto" }}>
+        <img src={appleStore} alt="apple store" className={classes.logo} />
+        <img src={googleStore} alt="google store" className={classes.logo} />
+      </Box>
+    </Box>
+  );
+};
+
+export default InfoHeader;
