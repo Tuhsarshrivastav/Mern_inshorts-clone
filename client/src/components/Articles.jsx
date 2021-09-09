2fb0af01f14325ae4725e4d27f055126bd79a9de
Tusharshrivastav <ts3657@gmail.com>
2021-09-09 18:01:46 +0530

    final fix and get ready to deploy

--- client/src/components/Articles.jsx
+++ client/src/components/Articles.jsx
@@ -1,42 +1,37 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { getNews } from "../services/api";
 
-import InfiniteScroll from 'react-infinite-scroll-component';
+import InfiniteScroll from "react-infinite-scroll-component";
 
 //components
-import Article from './Article';
+import Article from "./Article";
 
 const Articles = () => {
+  const [news, setNews] = useState([]);
+  const [page, setPage] = useState(0);
 
-    const [news, setNews] = useState([]);
-    const [page, setPage] = useState(0);
+  useEffect(() => {
+    const dailyNews = async () => {
+      const response = await getNews(page);
+      console.log(new Set([...news, ...response.data]));
+      setNews([...new Set([...news, ...response.data])]);
+    };
+    dailyNews();
+  }, [page]);
 
-    useEffect(() => {
-        const dailyNews = async () => {
-            const response = await getNews(page);
-            console.log(new Set([...news, ...response.data]));
-            setNews([...new Set([...news, ...response.data])]);
-        }
-        dailyNews();
-    }, [page])
+  useEffect(() => {}, [news]);
 
-    useEffect(() => {
-        console.log(news);
-    }, [news])
-
-    return (
-        <InfiniteScroll
-            dataLength={news.length}
-            next={() => setPage(page => page + 1)}
-            hasMore={true}
-        >
-            {
-                news.map(article => (
-                    <Article article={article} />
-                ))
-            }
-        </InfiniteScroll>
-    )
-}
+  return (
+    <InfiniteScroll
+      dataLength={news.length}
+      next={() => setPage((page) => page + 1)}
+      hasMore={true}
+    >
+      {news.map((article) => (
+        <Article article={article} />
+      ))}
+    </InfiniteScroll>
+  );
+};
 
 export default Articles;
